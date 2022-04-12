f26f6a3b19f80e4dc9083b5954443bae3def0d3a
yuusukekatsumata <103346797+yuusukekatsumata@users.noreply.github.com>
2022-04-13 01:16:13 +0900

    課題提出
    Java　1章　課題

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,32 @@
+public class Task1_1 {
+
+   public static void main(String[] args) {
+
+// ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+int hogeInt = 50;
+
+// ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+String hogeString = "テスト";
+
+// ③下記の処理について、何をしているのかコメントを記入してください
+// [ここへ記述]0.2を格納している
+double hogeDouble = 0.2;
+
+// ④変数 hogeInt を値「100」で上書き
+hogeInt = 100;
+			
+// ⑤変数 hogeString を値「合格」で上書き
+hogeString = "合格";
+    
+// ⑥変数 hogeInt の値を表示
+System.out.println(hogeInt);
+
+// ⑦下記の処理について、何をしているのかコメントを記入してください
+// [ここへ記述]上書き
+System.out.println(hogeString);
+
+// ⑧変数 hogeDouble の値を表示
+System.out.println(hogeDouble);
+
+}
+}
