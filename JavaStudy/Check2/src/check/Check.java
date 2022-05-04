8d9a53ae0ba8e69bd2f30dead15a92f6de888f8e
yuusukekatsumata <103346797+yuusukekatsumata@users.noreply.github.com>
2022-05-04 17:37:32 +0900

    課題提出
    java2章　チェックテスト

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,29 +1,32 @@
 package check;
 
+import constants.Constants;
+
 public class Check {
+	
 	//課題①
 	private static String firstName = "勝又";
     private static String lastName = "悠介" ;
     
-  //課題②
+    //課題②
     private static String printName(String firstName, String lastName) {
 			 return firstName + lastName;
 		  }
     
-public static void main(String[] args) {
+    public static void main(String[] args) {
 	
 	System.out.println("printNameメソッド→" + printName(firstName, lastName));
 	
-	
+    
+    
 	//課題③
-	
-	
-    Pet user1 = new Pet("java吉","hoge");
-    user1.introduce();
+
+    Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+    RobotPet robotoPet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
     
-    RobotPet user2 = new RobotPet("R2D2","ルーク");
-    user2.introduce();
+    pet.introduce();
+    robotoPet.introduce();
     
-    
-	}
+ }
 }
+             
